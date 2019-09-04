f94f8f8f526a4857e962f7361379a398e1292d4b
Justinas K <38762355+ibnzUK@users.noreply.github.com>
2019-09-04 18:32:45 +0100

    new 2 lines

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+lets start again 
+again555
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import web3 from './web3';
 
 function App() {
+ 
+    console.log(`web3 version is: `+ web3.version);
+    web3.eth.getAccounts().then(console.log);
+   
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload blamblam blam 2.
         </p>
         <a
           className="App-link"
